df128e9a13b1dc9826eaccd602079ab625ae1fd0
BennettDixon <bennettdixon16@gmail.com>
2018-12-03 21:28:42 +0000

    Removed need for string literals

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + " " + str[107:111] + " " + str[0:6]
+str = str[39:66] + str[6:7] + str[107:111] + str[6:7] + str[0:6]
 print(str)
