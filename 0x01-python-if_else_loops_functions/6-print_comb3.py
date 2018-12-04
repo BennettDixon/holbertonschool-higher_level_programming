13ca7465031ca824a03cdbe6ad134b5556fa95a3
BennettDixon <bennettdixon16@gmail.com>
2018-12-04 22:23:20 +0000

    printing unique combinations of numbers with python

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for d1 in range (0, 9):
+    for d2 in range (d1 + 1, 10):
+        print("{:d}{:d}".format(d1, d2), end='')
+        if d1 != 8:
+            print(", ", end='')
+        else:
+            print("")
