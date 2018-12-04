2fcad386f4fafc446b3d85325bb38611bd56fe30
BennettDixon <bennettdixon16@gmail.com>
2018-12-04 22:26:34 +0000

    Fixed pep8 styling

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for d1 in range (0, 9):
-    for d2 in range (d1 + 1, 10):
+for d1 in range(0, 9):
+    for d2 in range(d1 + 1, 10):
         print("{:d}{:d}".format(d1, d2), end='')
         if d1 != 8:
             print(", ", end='')
