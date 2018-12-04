fb2c0e7db72c0dcb3591d65f9c152792cf35184b
BennettDixon <bennettdixon16@gmail.com>
2018-12-04 19:55:53 +0000

    Using C to insert into a sorted linked list, preserving the sort

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,62 @@
+#include "lists.h"
+
+listint_t *create_node(int n);
+
+/**
+ * insert_node - inserts a node sorted in a linked list of ints
+ * @head: double pointer to head of LL, needed for modification in edge
+ * cases
+ * @number: data for new node
+ *
+ * Return: pointer to newly created node, NULL on failure
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *cur_node = NULL, *new_node = NULL;
+
+	if (!head)
+		return (NULL);
+	cur_node = *head;
+	while (cur_node)
+	{
+		/* need to insert at head */
+		if (cur_node->n > number)
+		{
+			new_node = create_node(number);
+			new_node->next = cur_node;
+			*head = new_node;
+			return (*head);
+		}
+		else if (cur_node->n < number)
+		{
+			if (!cur_node->next || cur_node->next->n > number)
+			{
+				new_node = create_node(number);
+				new_node->next = cur_node->next;
+				cur_node->next = new_node;
+				return (cur_node->next);
+			}
+		}
+		cur_node = cur_node->next;
+	}
+	return (NULL); /* failed */
+}
+
+
+/**
+ * create_node - creates a new node for the LL
+ * @n: data to insert into new node
+ *
+ * Return: pointer to newly allocated node
+ */
+listint_t *create_node(int n)
+{
+	listint_t *ret = NULL;
+
+	ret = malloc(sizeof(listint_t));
+	if (!ret)
+		return (NULL);
+	ret->next = NULL;
+	ret->n = n;
+	return (ret);
+}
