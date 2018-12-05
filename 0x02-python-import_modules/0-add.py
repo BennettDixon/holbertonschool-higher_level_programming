e46f75133037d0f4565fcff391fe8aac04c18d31
BennettDixon <bennettdixon16@gmail.com>
2018-12-05 20:33:33 +0000

    Restructured if block to not run on import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0 as calc
+from add_0 import add
 
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
