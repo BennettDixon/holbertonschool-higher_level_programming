cd672a3c37ebed955fa05d1435ff9d8cd811aa35
BennettDixon <bennettdixon16@gmail.com>
2018-12-05 16:36:44 +0000

    Using imports with renaming for easy use

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import calculator_1 as calc
+
+if __name__ != "__main__":
+    exit()
+
+a = 10
+b = 5
+
+print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
