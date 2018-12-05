40a927a0cf62e3e24291e77ea8120920a5ce01ef
BennettDixon <bennettdixon16@gmail.com>
2018-12-05 20:35:04 +0000

    restructed if block to run code only if not imported

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,13 +2,10 @@
 
 import calculator_1 as calc
 
-if __name__ != "__main__":
-    exit()
-
-a = 10
-b = 5
-
-print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
-print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
-print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
-print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
