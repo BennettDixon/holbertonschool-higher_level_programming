d9abe698a79e5cd480806c78f415362bfba793ba
BennettDixon <bennettdixon16@gmail.com>
2018-12-05 20:30:24 +0000

    Removed needless refrence to imported module symbol table

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import add_0 as calc
+import add_0.add as add
 
 if __name__ != "__main__":
     exit()
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
