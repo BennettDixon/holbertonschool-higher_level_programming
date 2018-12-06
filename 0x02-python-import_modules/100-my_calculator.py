62349de28d8121b2dfff958246e01b5a68ecfa7e
BennettDixon <bennettdixon16@gmail.com>
2018-12-06 22:42:53 +0000

    removed needless sys references and imported argv directly

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, sub, mul, div
 if __name__ != "__main__":
     exit()
 
-argc = len(sys.argv) - 1
+argc = len(argv) - 1
 if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
+    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
     exit(1)
-elif sys.argv[2] == '+':
+elif argv[2] == '+':
     func = add
-elif sys.argv[2] == '-':
+elif argv[2] == '-':
     func = sub
-elif sys.argv[2] == '*':
+elif argv[2] == '*':
     func = mul
-elif sys.argv[2] == '/':
+elif argv[2] == '/':
     func = div
+else:
+    print("Uknown operator. Available operators: +, -, *, and /")
+    exit(1)
 
-result = func(int(sys.argv[1]), int(sys.argv[3]))
-print("{:d} {:s} {:d} = {:d}".format(int(sys.argv[1]),
-sys.argv[2], int(sys.argv[3]), result))
+result = func(int(argv[1]), int(argv[3]))
+print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
+argv[2], int(argv[3]), result))
