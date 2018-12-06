f96e3427e49d4b1244808e902fb27c05b1fc4cec
BennettDixon <bennettdixon16@gmail.com>
2018-12-06 22:14:06 +0000

    Made more efficient by removing print exccess print statements

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,10 @@
 
 
 def uppercase(str):
+    cap_str = ""
     for c in str:
         temp = ord(c)
         if temp >= 97 and temp <= 122:
             temp -= 32
-        print("{:c}".format(temp), end='')
-    print()
+        cap_str += chr(temp)
+    print(cap_str)
