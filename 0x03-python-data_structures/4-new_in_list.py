7fe854157258545b9156fe6c83dedc42f9994036
BennettDixon <bennettdixon16@gmail.com>
2018-12-11 17:32:05 +0000

    Modifying a list without changing the original copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    """
+    Replaces an element in a list at a specific position
+    Without modifying the original list
+    """
+    l_len = len(my_list)
+    if idx >= l_len or idx < 0:
+        return
+
+    new_list = my_list[:]
+    new_list[idx] = element
+    return (new_list)
