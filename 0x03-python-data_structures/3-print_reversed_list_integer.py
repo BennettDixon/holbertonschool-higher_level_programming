c34219a6cc468a764dc5ca6a38cc7c4a9e6bad85
BennettDixon <bennettdixon16@gmail.com>
2018-12-12 00:27:06 +0000

    checking the type of provided variable to make sure list functions will work

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,7 +5,8 @@ def print_reversed_list_integer(my_list=[]):
     """
     prints the reverse of a given list
     """
-    new_l = my_list[0:]
-    new_l.reverse()
-    for i in range(len(new_l)):
-        print("{:d}".format(new_l[i]))
+    if type(my_list) is list:
+        new_l = my_list[0:]
+        new_l.reverse()
+        for i in range(len(new_l)):
+            print("{:d}".format(new_l[i]))
