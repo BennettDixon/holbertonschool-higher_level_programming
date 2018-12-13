83b4b5dfc504aa555c75fbfab23a6a97e3294011
BennettDixon <bennettdixon16@gmail.com>
2018-12-13 22:31:17 +0000

    Using map to perform operations on a list

---
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def mutiply_list_map(my_list=[], number=0):
+    """
+    performs a multiply function on a list by using map
+    """
+    return (list(map(lambda x: x*number, my_list))) 
