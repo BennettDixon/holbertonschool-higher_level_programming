44d926af573cf590f0556244a60de99c3a439c7f
BennettDixon <bennettdixon16@gmail.com>
2018-12-13 22:23:31 +0000

    finding highest value in a dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    """
+    gets the best value from a dictionary (greatest integer)
+    """
+    win_n = 0
+    winner = None
+    if type(a_dictionary) is dict:
+        for (key, value) in a_dictionary.items():
+            if value > win_n:
+                win_n = value
+                winner = key
+    return winner 
