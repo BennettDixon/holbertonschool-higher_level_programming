651692077b9d472471245dc7adc925084ecab049
BennettDixon <bennettdixon16@gmail.com>
2018-12-14 01:37:01 +0000

    fixed pep8 styling issue with lambdas

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return(list(map(lambda row : list(map(lambda x : x**2, row)), matrix)))
+    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
