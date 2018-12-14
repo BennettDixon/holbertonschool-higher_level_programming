ddf7d2b3c099ff163fb295d73420469e8cf14613
BennettDixon <bennettdixon16@gmail.com>
2018-12-14 01:43:37 +0000

    removed debug information

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,5 @@
 
 
 def weight_average(my_list=[]):
-    avg = 0
     scores = [score*weight for (score, weight) in my_list]
-    print(scores)
     return sum(scores) / sum([weight for (score, weight) in my_list])
