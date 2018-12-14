338d0c679560bceb0c67310016d7d2cb3bac07c4
BennettDixon <bennettdixon16@gmail.com>
2018-12-14 01:45:05 +0000

    added check for length of list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,5 +2,7 @@
 
 
 def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
     scores = [score*weight for (score, weight) in my_list]
     return sum(scores) / sum([weight for (score, weight) in my_list])
