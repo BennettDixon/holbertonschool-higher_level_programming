48ea078faa157eee8fb1e89a5a44f06165480163
BennettDixon <bennettdixon16@gmail.com>
2019-01-02 20:42:19 +0000

    printing elements of a list using try/except

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    list_len = 0
+    for ele in my_list:
+        list_len += 1
+    if (x > list_len):
+        x = list_len
+    try:
+        for i in range(0, x):
+            print(my_list[i], end="")
+        print()
+    except:
+        print("error occured")
+    finally:
+        return i + 1
