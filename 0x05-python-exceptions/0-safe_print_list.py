3ffd16959d8a50e8ba03ad0ad69639157e06dd2d
BennettDixon <bennettdixon16@gmail.com>
2019-01-02 20:43:49 +0000

    added print formatting to printing elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,9 +10,9 @@ def safe_print_list(my_list=[], x=0):
         x = list_len
     try:
         for i in range(0, x):
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
         print()
     except:
-        print("error occured")
+        print(end="")
     finally:
         return i + 1
