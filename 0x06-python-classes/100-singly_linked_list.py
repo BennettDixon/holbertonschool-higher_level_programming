9eb89796c07b0c6f3f0c8341deff07b8a0632eba
BennettDixon <bennettdixon16@gmail.com>
2019-01-03 21:33:11 +0000

    added docustrings for initialization functions and getters/setters

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,6 +6,9 @@ class Node:
     Node class for a singly linked list
     """
     def __init__(self, data, next_node=None):
+        """
+        initialization called when instance of class created
+        """
         if self.__validate_data(data):
             self.__data = data
         if self.__validate_node(next_node):
@@ -13,19 +16,31 @@ class Node:
 
     @property
     def data(self):
+        """
+        get the data attribute
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        set the data attribute
+        """
         if self.__validate_data(value):
             self.__data = value
 
     @property
     def next_node(self):
+        """
+        get the node attribute
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+        set the node attribute
+        """
         if self.__validate_node(value):
             self.__next_node = value
 
@@ -47,12 +62,16 @@ class Node:
             return True
         return False
 
+
 class SinglyLinkedList:
     """
     contains nodes for a singly linked list and methods for ->
     insertion
     """
     def __init__(self):
+        """
+        initialization called when instance of class created
+        """
         self.__head = None
 
     def __str__(self):
@@ -61,7 +80,7 @@ class SinglyLinkedList:
         """
         tmp = self.__head
         string = ""
-        while tmp != None:
+        while tmp is not None:
             string += str(tmp.data)
             tmp = tmp.next_node
             if tmp is not None:
@@ -74,12 +93,12 @@ class SinglyLinkedList:
                                             (based on data)
         """
         tmp = self.__head
-        if tmp == None:
+        if tmp is None:
             self.__head = Node(value)
             return
 
         prev = None
-        while tmp != None:
+        while tmp is not None:
             if (tmp.next_node is None or tmp.next_node.data >= value):
                 if (tmp.data >= value):
                     next_n = tmp
@@ -94,6 +113,6 @@ class SinglyLinkedList:
                     tmp.next_node = Node(value)
                     tmp.next_node.next_node = next_n
                 return
-            
+
             prev = tmp
             tmp = tmp.next_node
