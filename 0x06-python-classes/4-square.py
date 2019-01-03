796e4c16335e713a2c41b47d83fa17cccf999f94
BennettDixon <bennettdixon16@gmail.com>
2019-01-03 18:19:47 +0000

    using getters and setters in python using decoraters AKA the Pythonic way

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    class square that has attributes:
+        size
+    some attributes are protected from input.
+    """
+    def __init__(self, size=0):
+        """
+        initialization function for our square clasee
+        """
+        if self.__validate_size(size):
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if self.__validate_size(value):
+            self.__size = value
+
+    def area(self):
+        """
+        calculates the area of the square
+        """
+        return self.__size ** 2
+
+    def __validate_size(self, size):
+        """
+        validates the size, checking for errors
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            return True
+        return False
