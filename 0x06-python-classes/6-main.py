d880060c516a9e4ffcbbb4e0347dedc8a08973be
BennettDixon <bennettdixon16@gmail.com>
2019-01-03 18:39:12 +0000

    adding more getter/setters and modifying previous methods to use the new attributes

---
+++ 0x06-python-classes/6-main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+Square = __import__('6-square').Square
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
+
+my_square_4 = Square(3, "Holberton")
+my_square_4.my_print()
---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    class square that has attributes:
+        size
+    some attributes are protected from input.
+    """
+    def __init__(self, size=0, position=(0,0)):
+        """
+        initialization function for our square clasee
+        """
+        if self.__validate_size(size):
+            self.__size = size
+        if self.__validate_pos(position):
+            self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if self.__validate_size(value):
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if self.__validate_pos(value):
+            self.__position = value
+
+    def area(self):
+        """
+        calculates the area of the square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        prints the square using '#' characters
+        also takes into account position (x, y) offsets
+        """
+        i = 0
+        if self.__size == 0:
+            print()
+            return
+        for i in range(0, self.__position[1]):
+            print()
+        i = 0
+        for i in range(0, self.__size):
+            j = 0
+            x_pad = 0
+            for x_pad in range(0, self.__position[0]):
+                print(" ", end='')
+            for j in range(0, self.__size):
+                print("#", end='')
+            print()
+
+    def __validate_size(self, size):
+        """
+        validates the size, checking for errors
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            return True
+        return False
+
+    def __validate_pos(self, position):
+        """
+        validates the position, checking for type errors
+        """
+        if type(position) != type((0, 0)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+            return False
+        return True
