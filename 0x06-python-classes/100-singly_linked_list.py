d353f1b1b3f7a2601f477a101f228a6577a57e05
BennettDixon <bennettdixon16@gmail.com>
2019-01-03 21:28:24 +0000

    creating singly linked lists using classes, methods, attributes, and customization such as __str__

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+
+
+class Node:
+    """
+    Node class for a singly linked list
+    """
+    def __init__(self, data, next_node=None):
+        if self.__validate_data(data):
+            self.__data = data
+        if self.__validate_node(next_node):
+            self.__next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if self.__validate_data(value):
+            self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if self.__validate_node(value):
+            self.__next_node = value
+
+    def __validate_data(self, data):
+        """
+        validates the data, checking its type
+        Returns true or false if valid or not respectively
+        """
+        if isinstance(data, int):
+            return True
+        return False
+
+    def __validate_node(self, node):
+        """
+        validates the node, checking it's a node object
+        Returns true or false if valid or not respectively
+        """
+        if isinstance(node, Node) or node is None:
+            return True
+        return False
+
+class SinglyLinkedList:
+    """
+    contains nodes for a singly linked list and methods for ->
+    insertion
+    """
+    def __init__(self):
+        self.__head = None
+
+    def __str__(self):
+        """
+        used by print to print linked list
+        """
+        tmp = self.__head
+        string = ""
+        while tmp != None:
+            string += str(tmp.data)
+            tmp = tmp.next_node
+            if tmp is not None:
+                string += '\n'
+        return string
+
+    def sorted_insert(self, value):
+        """
+        inserts a new Node into the correct sorted position
+                                            (based on data)
+        """
+        tmp = self.__head
+        if tmp == None:
+            self.__head = Node(value)
+            return
+
+        prev = None
+        while tmp != None:
+            if (tmp.next_node is None or tmp.next_node.data >= value):
+                if (tmp.data >= value):
+                    next_n = tmp
+                    tmp = Node(value)
+                    tmp.next_node = next_n
+                    if prev is not None:
+                        prev.next_node = tmp
+                    else:
+                        self.__head = tmp
+                else:
+                    next_n = tmp.next_node
+                    tmp.next_node = Node(value)
+                    tmp.next_node.next_node = next_n
+                return
+            
+            prev = tmp
+            tmp = tmp.next_node
