7dd24dd6b6e336698dae8cd80b9d2af6215744dd
BennettDixon <bennettdixon16@gmail.com>
2019-01-03 18:13:24 +0000

    creating methods for class instances in python

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    class square that has attributes:
+        size
+    some attributes are protected from input.
+    """
+    def __init__(self, size=0):
+        """
+        initialization function for our square clasee
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """
+        calculates the area of the square
+        """
+        return self.__size ** 2
