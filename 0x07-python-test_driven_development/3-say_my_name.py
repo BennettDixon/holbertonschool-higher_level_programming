912f92bd047617b4f25200ea5f2d10b640c54b21
BennettDixon <bennettdixon16@gmail.com>
2019-01-07 17:53:40 +0000

    more unit testing practice with type checking

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    prints a hello string for a name, handling input errors
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
