d77100a5b4b140933325258e66b5b7553afb75e9
BennettDixon <bennettdixon16@gmail.com>
2019-01-07 18:05:51 +0000

    printing a square and doing value/type checking with unit testing

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def print_square(size):
+    """
+    prints a square of given size
+    unit tests are located in tests/4-print_square.txt
+    """
+    if not isinstance(size, (int, float)):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        if isinstance(size, float):
+            raise TypeError("size must be an integer")
+        else:
+            raise ValueError("size must be >= 0")
+
+    size = int(size) # if it was a float convert it
+    i = 0
+
+    for i in range(0, size):
+        j = 0
+        for j in range(0, size):
+            print('#', end='')
+        print()
