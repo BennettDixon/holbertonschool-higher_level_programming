eee8a54c765e84c2f349f0128ef35323ca7caff6
BennettDixon <bennettdixon16@gmail.com>
2019-01-09 16:39:29 +0000

    using data encapsulation with getters/setters (properties) in python

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+class Rectangle():
+    """rectangle class for storing rectangle data
+    """
+    def __init__(self, width=0, height=0):
+        """ instantiation method for object creation
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """ getter for height property """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter for height property """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        """ getter for width property """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setter for width property """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
