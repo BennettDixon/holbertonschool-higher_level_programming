518a2bebefc6c86f34cadde2b21ccf190be617ef
BennettDixon <bennettdixon16@gmail.com>
2019-01-16 21:19:08 +0000

    added docu string to class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,6 +4,8 @@
 Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
+    """square shape class, super class is BaseGeometry, then Rectangle
+    """
     def __init__(self, size):
         super().__init__(size, size)
         self.integer_validator("size", size)
