3df7b3d10fa84cc50e1ba768d8c1df2ce8c4f13c
BennettDixon <bennettdixon16@gmail.com>
2019-01-16 20:39:15 +0000

    fixed to work with only same class, not inherited classes like bools

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,7 +13,7 @@ class BaseGeometry():
     def integer_validator(self, name, value):
         """validates integer input
         """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError(name + " must be an integer")
         elif value <= 0:
             raise ValueError(name + " must be greater than 0") 
