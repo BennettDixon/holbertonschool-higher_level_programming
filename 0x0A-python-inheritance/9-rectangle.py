a0fcab7b3958ba45ab301b29e5a01054ca5978f4
BennettDixon <bennettdixon16@gmail.com>
2019-01-16 21:12:11 +0000

    custom __str__ methods as well as overriding parent methods

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class Rectangle(BaseGeometry):
+    """rectangle shape, inheirts from basegeometry
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """custom str method for str and print
+        """
+        string = "[Rectangle] "
+        string += str(self.__width) + '/' + str(self.__height)
+        return string
+
+    def area(self):
+        """overrides parent's method for area for use w/ rectangle
+        """
+        return (self.__width * self.__height) 
