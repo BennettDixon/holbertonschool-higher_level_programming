dec02e9fa6b6536d671ddb49760b17739f2130ea
BennettDixon <bennettdixon16@gmail.com>
2019-01-16 22:06:52 +0000

    checking if an object is a builtin, could be cleaner and cover more types, but not allowed to import

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def add_attribute(obj, name, value):
+    """adds an attribute to the class if possible
+        -> aka is
+    """
+    builtins = (str, int, complex, bool, float, list,\
+                tuple,dict, set, frozenset, type, object)
+    for _type in builtins:
+        if type(obj) is _type:
+            raise TypeError("can't add new attribute")
+
+    object.__setattr__(obj, name, value)
