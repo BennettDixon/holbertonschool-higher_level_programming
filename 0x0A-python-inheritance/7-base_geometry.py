c4d982cae3decc7e2deb752bf2895c06096b3f2d
BennettDixon <bennettdixon16@gmail.com>
2019-01-16 20:33:11 +0000

    implementing input validation w/ unit testing

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry():
+    """for use with shapes. Super class.
+    """
+    
+    def area(self):
+        """instance method to calculate area of shape
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates integer input
+        """
+        if not isinstance(value, int):
+            raise TypeError(name + " must be an integer")
+        elif value <= 0:
+            raise ValueError(name + " must be greater than 0") 
