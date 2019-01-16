d87049b3ca5eece1c084db6fd94b61fb642c077f
BennettDixon <bennettdixon16@gmail.com>
2019-01-16 20:23:23 +0000

    creating filler methods in OOP for later use w/ not implemented exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry():
+    """for use with shapes. Super class.
+    """
+    
+    def area(self):
+        """instance method to calculate area of shape
+        """
+        raise Exception("area() is not implemented")
