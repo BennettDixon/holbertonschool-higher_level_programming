29f690832995ce9b75f153811b3839b94c003c69
BennettDixon <bennettdixon16@gmail.com>
2019-01-16 20:15:12 +0000

    added check to make sure a_class passed is of type type

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,4 +5,6 @@ def is_same_class(obj, a_class):
     """checks if two objects are EXACTLY the same class
         -> doesn't care about inheritance
     """
+    if not isinstance(a_class, type):
+        raise TypeError("a_class must be of type 'type'")
     return (type(obj) is a_class)
