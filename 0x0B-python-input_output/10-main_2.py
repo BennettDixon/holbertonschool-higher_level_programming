260798374fd9715cbd728cac1474f277ad4cb7f3
BennettDixon <bennettdixon16@gmail.com>
2019-01-17 17:33:25 +0000

    test files for 10

---
+++ 0x0B-python-input_output/10-main_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+MyClass = __import__('10-my_class_2').MyClass
+class_to_json = __import__('10-class_to_json').class_to_json
+
+m = MyClass("John")
+m.win()
+print(type(m))
+print(m)
+
+mj = class_to_json(m)
+print(type(mj))
+print(mj)
+
