8a1d8e63a8bbcf1fef9967cb5675c6939815e531
BennettDixon <bennettdixon16@gmail.com>
2019-01-17 18:08:00 +0000

    more json serialization without use of json module

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+class Student:
+    """student class for use
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves dict rep of Student in json form
+        """
+        return {key: value for (key, value) in self.__dict__.items()\
+                if key in list(self.__dict__.keys())}
