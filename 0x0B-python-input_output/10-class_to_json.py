12c30f84bce7addc890c88d03839438e920f66f5
BennettDixon <bennettdixon16@gmail.com>
2019-01-17 17:48:46 +0000

    creating json object serialization of objects

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def class_to_json(obj):
+    """creates a json representation of an object without using
+        json module. One liner!! (without pep8 fix) :D
+    """
+    return repr({key: value for (key, value) in obj.__dict__.items()\
+                if key in list(obj.__dict__.keys())})
