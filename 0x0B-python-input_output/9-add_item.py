0274f7320b4b2062f7453afd4488e85675617907
BennettDixon <bennettdixon16@gmail.com>
2019-01-17 17:30:12 +0000

    adding items to a list representation of a json object by loading current copy then adding to it and writing json version back to file

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""module for taking arguments and adds to a list, saves to file
+"""
+
+
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+def main():
+    """contain main code in function
+    """
+    try:
+        new_list = load_from_json_file('add_item.json')
+    except: # file didn't exist to read from
+        new_list = []
+
+    new_list.extend([sys.argv[i] for i in range(0, len(sys.argv)) if i != 0])
+    try:
+        save_to_json_file(new_list, 'add_item.json')
+    except:
+        pass
+
+main()
