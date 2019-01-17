a4e47aedb4ec88b99844423a9a81a2bee0d84ea8
BennettDixon <bennettdixon16@gmail.com>
2019-01-17 17:50:20 +0000

    testing other things like lists and lists of lists

--- 0x0B-python-input_output/10-my_class.py
+++ 0x0B-python-input_output/10-my_class.py
@@ -9,6 +9,7 @@ class MyClass:
     def __init__(self, name):
         self.name = name
         self.number = 0
-
+        self.lists = ["hi", "there", 4, "list"]
+        self.listoflists = [[-1, 0], [1, 2]]
     def __str__(self):
         return "[MyClass] {} - {:d}".format(self.name, self.number)
