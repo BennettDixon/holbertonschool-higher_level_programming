ce1bc79de456dba297df39adb2fee13f0a2b621c
BennettDixon <bennettdixon16@gmail.com>
2019-01-17 16:23:58 +0000

    docu string added for function

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -2,5 +2,7 @@
 
 
 def number_of_lines(filename=""):
+    """gets the number of lines from filename
+    """
     with open(filename, encoding='utf-8') as myFile:
         return sum([1 for line in myFile])
