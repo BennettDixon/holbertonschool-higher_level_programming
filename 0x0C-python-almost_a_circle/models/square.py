5aeb3c3b8a9ae2248a5a9061da45e1595e89afd7
BennettDixon <bennettdixon16@gmail.com>
2019-01-22 19:35:45 +0000

    implementing update method for square, overrides parent

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,6 +21,32 @@ class Square(Rectangle):
                                                     self.width)
         return builder
 
+    def update(self, *args, **kwargs):
+        """takes an *args argument and sets arguments respective
+            to instantiation function
+        """
+        if args != None and len(args) > 0:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+        elif kwargs is not None:
+            for (key, value) in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
+
     @property
     def size(self):
         return self.width
