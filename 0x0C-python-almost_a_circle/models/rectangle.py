7dd3f38e42e5970ba01547ffb37318b9bd4798a5
BennettDixon <bennettdixon16@gmail.com>
2019-01-22 17:43:05 +0000

    using key-worded arguments to update a class's attributes, improved update method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -44,21 +44,34 @@ class Rectangle(Base):
                 builder += "#"
             print(builder)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """takes an *args argument and sets arguments respective
             to instantiation function
         """
-        for i, arg in enumerate(args):
-            if i == 0:
-                self.id = arg
-            elif i == 1:
-                self.width = arg
-            elif i == 2:
-                self.height = arg
-            elif i == 3:
-                self.x = arg
-            elif i == 4:
-                self.y = arg
+        if args != None and len(args) > 0:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.width = arg
+                elif i == 2:
+                    self.height = arg
+                elif i == 3:
+                    self.x = arg
+                elif i == 4:
+                    self.y = arg
+        elif kwargs is not None:
+            for (key, value) in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
 
     @property
     def width(self):
