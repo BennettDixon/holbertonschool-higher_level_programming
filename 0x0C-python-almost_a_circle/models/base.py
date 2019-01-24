01358da062884f90dea4718882bfaff4b2eb0166
BennettDixon <bennettdixon16@gmail.com>
2019-01-24 16:13:40 +0000

    added method to base model for coverting list of dicts to json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,3 +16,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns JSON string representation of list_dictionaries
+        """
+        return str(list_dictionaries)
