13abfc30352a21d42aecb462a1732c6a76654679
BennettDixon <bennettdixon16@gmail.com>
2019-03-05 22:12:05 +0000

    using multiple joins to extract information across many tables

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,12 @@
+-- script for selecting TV shows and their genres
+-- join statement to do result DML
+SELECT
+  tv_genres.name AS genre
+  FROM tv_show_genres
+  INNER JOIN tv_genres
+  ON tv_show_genres.genre_id = tv_genres.id
+  INNER JOIN tv_shows
+  ON tv_shows.id = tv_show_genres.show_id
+  AND tv_shows.title = 'Dexter'
+  GROUP BY genre
+  ORDER BY genre;
