cea71a55c582d3add77e88da8fce974d00dff61b
BennettDixon <bennettdixon16@gmail.com>
2019-03-05 20:16:04 +0000

    Added comments to sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,3 @@
+-- using join to display info about two tables
+-- statement to do join query
 SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC;
