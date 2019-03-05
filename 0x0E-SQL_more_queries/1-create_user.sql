1b4fbe1b25eac6d674bf5b2fcfd4ab3e30022990
BennettDixon <bennettdixon16@gmail.com>
2019-03-05 18:02:28 +0000

    creating users and granting root perms in SQL

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- script to create a new user in server
+-- DDL query to do so.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
