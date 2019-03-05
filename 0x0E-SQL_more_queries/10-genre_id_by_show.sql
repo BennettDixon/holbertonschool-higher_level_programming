61256bee03cc02cafe650b174cf8f1386d658b5c
BennettDixon <bennettdixon16@gmail.com>
2019-03-05 19:46:59 +0000

    added comments and added new lines for readability

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,1 +1,6 @@
-SELECT tv_shows.title,tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id ORDER BY tv_shows.title,tv_show_genres.genre_id ASC;
+-- script for selecting TV shows and their genres
+-- join statement to do result DML
+SELECT tv_shows.title, tv_show_genres.genre_id
+  FROM tv_shows INNER JOIN tv_show_genres
+  ON tv_shows.id = tv_show_genres.show_id
+  ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
