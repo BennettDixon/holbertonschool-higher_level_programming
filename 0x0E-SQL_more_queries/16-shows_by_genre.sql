065dc83308fff93d2c059279c08c1a8cddc62b04
BennettDixon <bennettdixon16@gmail.com>
2019-03-05 22:41:01 +0000

    using multiple joins with different orientations to filer data and show empty results

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- script for selecting TV shows and their genres
+-- join statement to do result DML
+SELECT
+  tv_shows.title AS title,
+  tv_genres.name AS name
+  FROM tv_show_genres
+  RIGHT JOIN tv_shows
+  ON tv_show_genres.show_id = tv_shows.id
+  LEFT JOIN tv_genres
+  ON tv_genres.id = tv_show_genres.genre_id
+  ORDER BY title;
