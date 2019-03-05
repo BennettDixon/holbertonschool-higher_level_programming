482cb517bbaac5e8a6a7492dd4597d859dbcd817
BennettDixon <bennettdixon16@gmail.com>
2019-03-05 18:03:27 +0000

    added query comment

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- script to create a new user in server
 -- DDL query to do so.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- query to grant perms to user
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
