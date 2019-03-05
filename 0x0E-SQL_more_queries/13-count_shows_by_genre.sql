5a73c34f0c619086b46293333c61fb7dce835b18
BennettDixon <bennettdixon16@gmail.com>
2019-03-05 21:37:22 +0000

    using aggregate functions in JOIN statements to gather information on tables

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- script for selecting TV shows and their genres
+-- join statement to do result DML
+SELECT
+  tv_genres.name AS genre,
+  COUNT(tv_show_genres.genre_id) AS number_of_shows
+  FROM tv_show_genres
+  INNER JOIN tv_genres
+  ON tv_show_genres.genre_id = tv_genres.id
+  GROUP BY genre
+  ORDER BY COUNT(tv_show_genres.genre_id) DESC;
