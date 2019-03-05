1ab8549bed5055f986c2347ce53349f2f7efbbe7
BennettDixon <bennettdixon16@gmail.com>
2019-03-05 00:54:33 +0000

    throwing strange error, but appears to be working conversion to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,6 @@
 -- script to convert db to utf-8 encoding
 -- DDL query to do conversion
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY name BLOB;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
