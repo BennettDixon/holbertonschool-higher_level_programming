3b6abe08f2beb8f7e5602cd6b21bd01ca3f1525a
BennettDixon <bennettdixon16@gmail.com>
2019-03-05 19:13:22 +0000

    making sql dynamic and not rely on ID of state

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- grab all cities with state id of california (1)
 -- DML query to do so
-SELECT id,name FROM cities WHERE state_id = 1;
+SELECT id,name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California');
