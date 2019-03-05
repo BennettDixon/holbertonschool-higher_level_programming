8a3cf7fa31c1aaf3d2165567930d2466243254aa
BennettDixon <bennettdixon16@gmail.com>
2019-03-05 19:48:10 +0000

    using LEFT JOIN to show all results in left table regardless if match in right

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- script for selecting TV shows and their genres
+-- join statement to do result DML
+SELECT tv_shows.title, tv_show_genres.genre_id
+  FROM tv_shows LEFT JOIN tv_show_genres
+  ON tv_shows.id = tv_show_genres.show_id
+  ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
