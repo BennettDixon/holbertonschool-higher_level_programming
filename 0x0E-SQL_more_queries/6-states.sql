0a57099c6a2548722ae6bb5858f706d7d3d82e41
BennettDixon <bennettdixon16@gmail.com>
2019-03-05 18:45:26 +0000

    adding comments for each SQL query

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,9 @@
 -- script to create a table in server
 -- DDL query to do so
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- set db to new db
 USE hbtn_0d_usa;
+-- create new table
 CREATE TABLE IF NOT EXISTS states(
   id INT NOT NULL AUTO_INCREMENT UNIQUE,
   name VARCHAR(256) NOT NULL,
