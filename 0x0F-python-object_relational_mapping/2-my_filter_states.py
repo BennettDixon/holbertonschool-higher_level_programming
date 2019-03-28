500a289ec97ebed16d10e7da78fa7b9652749b53
BennettDixon <bennettdixon16@gmail.com>
2019-03-28 16:06:22 +0000

    python script using MySQLdb to filter state names based on user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""script for use in getting all states from sql db
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) < 5:
+        print("Usage: {} username password db_name state_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    state_name = args[4]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=data,
+                         port=3306)
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        if (state_name in row[1]):
+            print(row)
