c7a1e4863d8d59aebfe8e0d35b635b5d9c0c6a22
BennettDixon <bennettdixon16@gmail.com>
2019-03-28 15:34:39 +0000

    using MySQLdb to execute queries after connecting to a database using python

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""script for use in getting all states from sql db
+"""
+import MySQLdb
+import sys
+
+
+args = sys.argv
+if len(args) < 4:
+    print("Usage: {} username password database_name".format(args[0]))
+    exit(1)
+username = args[1]
+password = args[2]
+data = args[3]
+db = MySQLdb.connect(host='localhost', user=username,
+                     passwd=password, db=data,
+                     port=3306)
+cur = db.cursor()
+num_rows = cur.execute("SELECT * FROM states")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
