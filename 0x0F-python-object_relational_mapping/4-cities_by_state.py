071ae4d4de0d61973fd972f0dd70546d3b68ee8a
BennettDixon <bennettdixon16@gmail.com>
2019-03-28 16:26:56 +0000

    using joins in sql coupled with python to do powerful oop database mapping

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""script for use in getting all states from sql db
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) < 4:
+        print("Usage: {} username password db_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=data,
+                         port=3306)
+    cur = db.cursor()
+    num_rows = cur.execute('''
+        SELECT states.id, cities.name, states.name
+        FROM cities INNER JOIN states
+        ON cities.state_id=states.id
+        ORDER BY states.id ASC
+        ''')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
