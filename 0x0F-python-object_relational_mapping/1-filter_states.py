da3a91cbefa1478627a0ddb061ec368fa714e0f0
BennettDixon <bennettdixon16@gmail.com>
2019-03-28 15:42:13 +0000

    fixed code to not run when imported, only run when main file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,19 +5,20 @@ import MySQLdb
 import sys
 
 
-args = sys.argv
-if len(args) < 4:
-    print("Usage: {} username password database_name".format(args[0]))
-    exit(1)
-username = args[1]
-password = args[2]
-data = args[3]
-db = MySQLdb.connect(host='localhost', user=username,
-                     passwd=password, db=data,
-                     port=3306)
-cur = db.cursor()
-num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
-rows = cur.fetchall()
-for row in rows:
-    if (row[1][0] == 'N'):
-        print(row)
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) < 4:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=data,
+                         port=3306)
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        if (row[1][0] == 'N'):
+            print(row)
