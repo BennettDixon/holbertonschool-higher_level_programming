0e8d8edd31df26112081c111881d3e7576873bea
BennettDixon <bennettdixon16@gmail.com>
2019-03-29 16:37:02 +0000

    fixed pep8 styling

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """state class for use with sqlalchemy
         -> inherits from sqlalchemy declarative_base
