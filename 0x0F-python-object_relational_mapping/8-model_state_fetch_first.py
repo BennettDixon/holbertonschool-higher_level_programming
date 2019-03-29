a28c918f9739b7ac2455311b207f42bee6102ba5
BennettDixon <bennettdixon16@gmail.com>
2019-03-29 17:41:10 +0000

    fixed pep8 styling for None comparison

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     # create instance of new custom session class
     session = Session()
     state = session.query(State).order_by(State.id).first()
-    if (state != None):
+    if (state is not None):
         print('{}: {}'.format(state.id, state.name))
     else:
         print('Nothing')
