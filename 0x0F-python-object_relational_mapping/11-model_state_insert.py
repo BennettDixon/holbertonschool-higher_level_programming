e5222b7d7f7b59c5f7ba58bffcc96b52c1ebfcfc
BennettDixon <bennettdixon16@gmail.com>
2019-03-29 18:26:23 +0000

    inserting values into sql databases using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script to list all state objects using sqlalchemy
+"""
+from model_state import Base, State
+
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+import sys
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # create custom session object class from database engine
+    Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
+    session = Session()
+    new_state = State()
+    new_state.name = 'Louisiana'
+    session.add(new_state)
+    session.commit()
