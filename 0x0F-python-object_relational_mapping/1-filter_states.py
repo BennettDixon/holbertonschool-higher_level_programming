e5d2d33d76df76b1a1863d0acc7bbe5535d28575
BennettDixon <bennettdixon16@gmail.com>
2019-03-29 15:51:54 +0000

    removed argument validation because specified not needed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,9 +7,6 @@ import sys
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) < 4:
-        print("Usage: {} username password database_name".format(args[0]))
-        exit(1)
     username = args[1]
     password = args[2]
     data = args[3]
