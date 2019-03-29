ba03bd53cce6856132a0391541497f2e510ef802
BennettDixon <bennettdixon16@gmail.com>
2019-03-29 16:36:20 +0000

    first orm model for sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script for using sqlalchemy to model our models using ORM
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """state class for use with sqlalchemy
+        -> inherits from sqlalchemy declarative_base
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
