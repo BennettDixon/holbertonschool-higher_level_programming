569a12671a16d47b82ab96c4ce8a7a920216ff4c
BennettDixon <bennettdixon16@gmail.com>
2019-03-29 19:25:56 +0000

    deleting objects from sql storage using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script to list all state objects using sqlalchemy
+"""
+from model_state import Base, State
+
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+import sys
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # create custom session object class from database engine
+    Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
+    session = Session()
+    states = session.query(State).filter(State.name.contains('a'))
+    for state in states:
+        session.delete(state)
+    session.commit()
