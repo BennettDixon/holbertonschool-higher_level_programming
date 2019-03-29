648eea9acd1a8158b2b5e4f915cac54a3b97a2c0
BennettDixon <bennettdixon16@gmail.com>
2019-03-29 19:50:01 +0000

    using joins in sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script to list all state objects using sqlalchemy
+"""
+from model_state import Base, State
+
+from model_city import City
+
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+import sys
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # create custom session object class from database engine
+    Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
+    session = Session()
+    for result in session.query(State.name, City.id, City.name)\
+                      .join(City, City.state_id == State.id)\
+                      .order_by(State.id):
+        print("{}: ({}) {}".format(result[0], result[1], result[2]))
