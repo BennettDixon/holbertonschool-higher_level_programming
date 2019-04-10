5e261845267c0e9390a70edaf10b818f09ea8819
BennettDixon <bennettdixon16@gmail.com>
2019-04-10 19:24:43 +0000

    fixed shebang to holberton's req's

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # bash script to send a get request to the url and display body of resposne
 # shellcheck disable=SC2046
 if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
