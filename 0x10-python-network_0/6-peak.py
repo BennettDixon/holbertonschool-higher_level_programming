3ddc25d90c2b552f57ec9423fae7f7b43ac97424
BennettDixon <bennettdixon16@gmail.com>
2019-04-10 20:44:42 +0000

    brute force solution for interview prep question, going to work on finding better methods

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""script for finding peak in list of ints, interview prep
+"""
+
+"""
+    THOUGHT PROCESS
+        it is not sorted, so sorting would take n(log(n))
+            -> not worth sorting
+        looping through and keeping track of max (brute force)
+            -> O(n)
+
+        possibly looping from each end reducing to 1/2 run time
+            -> still O(n)
+"""
+
+def find_peak(list_of_integers):
+    """BRUTE force implementation for question
+    """
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
