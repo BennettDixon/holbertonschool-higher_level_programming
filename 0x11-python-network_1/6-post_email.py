ab8a641d30069194548d3f1fdf0d2d49ea81b984
BennettDixon <bennettdixon16@gmail.com>
2019-04-15 16:18:08 +0000

    posting data using the requests library

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""script for posting data to web servers
+"""
+import requests
+import sys
+url = sys.argv[1]
+email = sys.argv[2]
+payload = {'email': email}
+response = requests.post(url, data=payload)
+print(response.text)
