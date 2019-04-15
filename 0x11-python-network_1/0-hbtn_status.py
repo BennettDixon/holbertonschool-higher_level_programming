34c340c50adfb3d480603bad26653a0e5e059547
BennettDixon <bennettdixon16@gmail.com>
2019-04-15 19:07:24 +0000

    used tabs instead of spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ with urllib.request.urlopen(url) as response:
     bytes_content = response.read()
     content = bytes_content.decode('utf-8')
     print_str = '''Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}'''.format(type(bytes_content), bytes_content, content)
+\t- type: {}
+\t- content: {}
+\t- utf8 content: {}'''.format(type(bytes_content), bytes_content, content)
     print(print_str)
