fce7273090488cb59aef5cbf9fb538303a8e7355
BennettDixon <bennettdixon16@gmail.com>
2019-04-15 19:39:20 +0000

    printed the acual response instead of passing if opened successfully

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as resp:
-            pass
+            print(resp.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
