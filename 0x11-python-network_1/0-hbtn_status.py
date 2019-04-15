d32c2eae57028b19d804ba86fa85714a38129098
BennettDixon <bennettdixon16@gmail.com>
2019-04-15 19:41:59 +0000

    removed spaces from tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         bytes_content = response.read()
         content = bytes_content.decode('utf-8')
         print_str = '''Body response:
-    \t- type: {}
-    \t- content: {}
-    \t- utf8 content: {}'''.format(type(bytes_content), bytes_content, content)
+\t- type: {}
+\t- content: {}
+\t- utf8 content: {}'''.format(type(bytes_content), bytes_content, content)
         print(print_str)
