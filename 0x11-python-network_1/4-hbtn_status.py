dcd4f0abaa93a77219fe11e88e3ff0e599bdf7e8
BennettDixon <bennettdixon16@gmail.com>
2019-04-15 19:28:01 +0000

    added import guard

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """script for testing status of web pages
 """
-import requests
-url = "https://intranet.hbtn.io/status"
-with requests.get(url) as response:
-    content = response.text
-    print_str = '''Body response:
-\t- type: {}
-\t- content: {}'''.format(type(content), content)
-    print(print_str)
+if __name__ == "__main__":
+    import requests
+    url = "https://intranet.hbtn.io/status"
+    with requests.get(url) as response:
+        content = response.text
+        print_str = '''Body response:
+    \t- type: {}
+    \t- content: {}'''.format(type(content), content)
+        print(print_str)
