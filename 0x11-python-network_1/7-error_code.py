a5408ad8090bfe495d3dd4c020e85f3eb9e1a601
BennettDixon <bennettdixon16@gmail.com>
2019-04-15 19:36:27 +0000

    protected from imports

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """script for posting data to web servers
 """
-import requests
-import sys
-url = sys.argv[1]
-response = requests.get(url)
-if response.status_code != requests.codes.ok:
-    print('Error code: {}'.format(response.status_code))
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code != requests.codes.ok:
+        print('Error code: {}'.format(response.status_code))
