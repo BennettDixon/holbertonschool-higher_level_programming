06186fc3b29fc873a1e5602dfd94dced3f1709d7
BennettDixon <bennettdixon16@gmail.com>
2019-04-15 15:32:59 +0000

    posting data to end points

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script for testing POST requests so servers
+"""
+import urllib.request
+import urllib.parse
+import sys
+url = sys.argv[1]
+email = sys.argv[2]
+payload = {'email': email}
+payload = urllib.parse.urlencode(payload)
+payload = payload.encode('ascii')
+req = urllib.request.Request(url, payload)
+with urllib.request.urlopen(req) as response:
+    print(response.read())
