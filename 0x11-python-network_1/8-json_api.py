ccb9c9237f25284d5fdb7e89dfa2f53d9921e219
BennettDixon <bennettdixon16@gmail.com>
2019-04-15 20:06:35 +0000

    querying an api and gathering data as json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script for posting data to web servers
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    query = {'q': q}
+    response = requests.post(url, data=query)
+    if response.status_code != requests.codes.ok or len(response.text) <= 0:
+        print('No result')
+        sys.exit()
+    else:
+        try:
+            my_obj = eval(response.text)
+            if len(my_obj) == 0:
+                print('No result')
+                sys.exit()
+            my_id = my_obj.get('id')
+            my_name = my_obj.get('name')
+            print("[{}] {}".format(my_id, my_name))
+        except Exception as E:
+            print('Not a valid JSON')
