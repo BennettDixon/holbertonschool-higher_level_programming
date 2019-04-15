6bffde5a400073596063b7c99ef73e1a1f19b348
BennettDixon <bennettdixon16@gmail.com>
2019-04-15 19:21:03 +0000

    added import guard for script

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """script for testing status of web pages
 """
-import urllib.request
-import sys
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    meta = response.info()
-    for header in meta._headers:
-        if header[0] == 'X-Request-Id':
-            print(header[1])
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        meta = response.info()
+        for header in meta._headers:
+            if header[0] == 'X-Request-Id':
+                print(header[1])
