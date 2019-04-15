e47bb8d4981cc479f1c9ffc8a92faa2a6e54e374
BennettDixon <bennettdixon16@gmail.com>
2019-04-15 19:35:36 +0000

    protected sciprt from import

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """script for posting data to web servers
 """
-import requests
-import sys
-url = sys.argv[1]
-email = sys.argv[2]
-payload = {'email': email}
-response = requests.post(url, data=payload)
-print(response.text)
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    print(response.text)
