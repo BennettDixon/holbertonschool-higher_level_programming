70cf013971ad21bd2ef2bcc32718af2204803969
BennettDixon <bennettdixon16@gmail.com>
2019-05-07 15:46:55 +0000

    semistandard js styling fixes

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,26 +1,25 @@
 #!/usr/bin/node
 const process = require('process');
-let build = "";
+let build = '';
 let failed = 'Missing number of occurrences';
 let num;
 let i;
-
 
 if (process.argv.length > 2) {
   num = parseInt(process.argv[2]);
   if (isNaN(num)) {
     build = failed;
   } else {
-      for (i = 0; i < num; i++) {
-        if (i > 0) {
-          build += '\n';
-        }
-        build += 'C is fun';
+    for (i = 0; i < num; i++) {
+      if (i > 0) {
+        build += '\n';
       }
+      build += 'C is fun';
+    }
   }
 } else {
   build = failed;
 }
-if (build != "") {
+if (build !== '') {
   console.log(build);
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,29 +1,28 @@
 #!/usr/bin/node
 const process = require('process');
-let build = "";
+let build = '';
 let failed = 'Missing size';
 let num;
 let i;
 let j;
-
 
 if (process.argv.length > 2) {
   num = parseInt(process.argv[2]);
   if (isNaN(num)) {
     build = failed;
   } else {
-      for (i = 0; i < num; i++) {
-        if (i > 0) {
-          build += '\n';
-        }
-        for (j = 0; j < num; j++) {
-          build += 'X';
-        }
+    for (i = 0; i < num; i++) {
+      if (i > 0) {
+        build += '\n';
       }
+      for (j = 0; j < num; j++) {
+        build += 'X';
+      }
+    }
   }
 } else {
   build = failed;
 }
-if (build != "") {
+if (build !== '') {
   console.log(build);
 }
