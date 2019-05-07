ab2d0a5b165a617a86cef378c27d3824d5d65d00
BennettDixon <bennettdixon16@gmail.com>
2019-05-07 19:46:57 +0000

    filtered output for one argument using plural or nonplural

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const process = require('process');
 let resp;
-if (process.argv.length > 2) {
-  resp = 'Arguments found';
-} else {
+if (process.argv.length === 3) {
+  resp = 'Argument found';
+} else if (process.argv.length < 3) {
   resp = 'No argument';
+} else {
+  resp = 'Arguments found';
 }
 console.log(resp);
