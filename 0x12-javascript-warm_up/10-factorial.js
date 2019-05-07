25f3a1870320ff8ce1bb38bd86993c91f2deb6d6
BennettDixon <bennettdixon16@gmail.com>
2019-05-07 19:52:30 +0000

    calculating factorials using recursion in js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const process = require('process');
+
+function factorialize(num) {
+  if (isNaN(num)) {
+    return 1;
+  }
+  if (num < 0) {
+    return -1;
+  }
+  else if (num == 0) {
+    return 1;
+  }
+  else {
+    return (num * factorialize(num - 1));
+  }
+}
+
+console.log(factorialize(parseInt(process.argv[2])));
