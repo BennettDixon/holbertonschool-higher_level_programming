4c3c3dff3777bfdccb77bc22e663622e28e0762e
BennettDixon <bennettdixon16@gmail.com>
2019-05-07 15:14:35 +0000

    using foreach loops and string concatination to filter args

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const process = require('process');
+
+let resp;
+resp = "";
+process.argv.forEach(function(value, index){
+	if (index > 1) {
+		if (index > 2) {
+			resp += ' ';
+		}
+		resp += value;
+	}
+});
+console.log(resp);
