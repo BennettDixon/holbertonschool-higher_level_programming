3978933ff49bda0bb47288ec1bb3ab051cd78092
BennettDixon <bennettdixon16@gmail.com>
2019-05-07 20:03:28 +0000

    finding the second largest integer in a list provided

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const process = require('process');
+let max;
+let secondMax;
+
+process.argv.forEach(function (value, index) {
+  if (index > 1) {
+    if (max === undefined) {
+      max = value;
+    } else if (secondMax === undefined && value < max) {
+      secondMax = value;
+    } else if (value > max) {
+      secondMax = max;
+      max = value;
+    }
+  }
+});
+if (secondMax === undefined) {
+  console.log(0);
+} else {
+  console.log(secondMax);
+}
