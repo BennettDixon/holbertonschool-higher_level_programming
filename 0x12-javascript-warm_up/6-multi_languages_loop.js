00d75d3fa20d427dc96b9dad08858b2f0417b409
BennettDixon <bennettdixon16@gmail.com>
2019-05-07 19:43:34 +0000

    using a for loop to do it instead of foreach w/ builder

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 
 let languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let resp = '';
-languages.forEach(function (value, index) {
-  if (index > 0) {
-    resp += '\n';
-  }
-  resp += value;
-});
-console.log(resp);
+for (i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
