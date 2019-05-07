a448a6baec0200cc2e989f98f129901241b473c4
BennettDixon <bennettdixon16@gmail.com>
2019-05-07 15:28:50 +0000

    looping through more arrays

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let resp = "";
+languages.forEach(function(value, index){
+	if (index > 0) {
+		resp += '\n';
+	}
+	resp += value;
+});
+console.log(resp);
