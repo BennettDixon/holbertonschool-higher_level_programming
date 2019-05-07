344f91ae72097436c2c19417dbabdf03183f72b5
BennettDixon <bennettdixon16@gmail.com>
2019-05-07 15:25:39 +0000

    type conversions in javascript

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+let resp;
+let num;
+resp = "Not a number";
+if (process.argv.length > 2) {
+	num = parseInt(process.argv[2]);
+	if (!isNaN(num)) {
+		resp = "My number: " + String(num);
+	}
+}
+console.log(resp);
