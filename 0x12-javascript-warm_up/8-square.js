e681e3bd02df4b3ef5ba7d875d3b76666b274ca3
BennettDixon <bennettdixon16@gmail.com>
2019-05-07 15:42:59 +0000

    more text processing using nested loops in javascript

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+const process = require('process');
+let build = "";
+let failed = 'Missing size';
+let num;
+let i;
+let j;
+
+
+if (process.argv.length > 2) {
+  num = parseInt(process.argv[2]);
+  if (isNaN(num)) {
+    build = failed;
+  } else {
+      for (i = 0; i < num; i++) {
+        if (i > 0) {
+          build += '\n';
+        }
+        for (j = 0; j < num; j++) {
+          build += 'X';
+        }
+      }
+  }
+} else {
+  build = failed;
+}
+if (build != "") {
+  console.log(build);
+}
