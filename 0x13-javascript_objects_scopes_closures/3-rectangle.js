65ee69d55997f4800337fbd4e5e4065ecdf337a4
BennettDixon <bennettdixon16@gmail.com>
2019-05-13 17:38:39 +0000

    removed needless var

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,11 +7,10 @@ class Rectangle {
     }
   }
 
-  print() {
-    let num;
+  print () {
     let i;
     let j;
-    let build = "";
+    let build = '';
     for (i = 0; i < this.height; i++) {
       if (i > 0) {
         build += '\n';
