d6541131314a545beb7c88ef705282c87b5772b7
BennettDixon <bennettdixon16@gmail.com>
2019-05-13 17:27:00 +0000

    custom instance class methods

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    let num;
+    let i;
+    let j;
+    let build = "";
+    for (i = 0; i < this.height; i++) {
+      if (i > 0) {
+        build += '\n';
+      }
+      for (j = 0; j < this.width; j++) {
+        build += 'X';
+      }
+    }
+    console.log(build);
+  }
+}
+module.exports = Rectangle;
