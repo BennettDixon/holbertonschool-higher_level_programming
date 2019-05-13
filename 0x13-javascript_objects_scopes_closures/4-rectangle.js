85e5e50761b7b5bef1356608d646806cf16ee1ff
BennettDixon <bennettdixon16@gmail.com>
2019-05-13 17:29:35 +0000

    modifying instance attributes in javascript from instance methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    let num;
+    let i;
+    let j;
+    let build = "";
+    for (i = 0; i < this.height; i++) {
+      if (i > 0) {
+        build += '\n';
+      }
+      for (j = 0; j < this.width; j++) {
+        build += 'X';
+      }
+    }
+    console.log(build);
+  }
+
+  rotate() {
+    let tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
