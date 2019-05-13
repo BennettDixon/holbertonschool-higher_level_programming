59841ff221c38ac809e4d42a6f5c20708e8117bb
BennettDixon <bennettdixon16@gmail.com>
2019-05-13 17:56:57 +0000

    functions are objects in javascript

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.logMe = function logMe(item) {
+  if (typeof logMe.counter == 'undefined') {
+    logMe.counter = 0;
+  }
+  console.log(logMe.counter + ': ' + item)
+  logMe.counter++;
+}
