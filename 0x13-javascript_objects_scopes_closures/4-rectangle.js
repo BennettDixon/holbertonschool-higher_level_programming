d52533d9db4c3552a570d229fa7085728471da23
BennettDixon <bennettdixon16@gmail.com>
2019-05-13 17:38:50 +0000

    added optional print char for print

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -7,29 +7,28 @@ class Rectangle {
     }
   }
 
-  print() {
-    let num;
+  print (print_c = 'X') {
     let i;
     let j;
-    let build = "";
+    let build = '';
     for (i = 0; i < this.height; i++) {
       if (i > 0) {
         build += '\n';
       }
       for (j = 0; j < this.width; j++) {
-        build += 'X';
+        build += print_c;
       }
     }
     console.log(build);
   }
 
-  rotate() {
+  rotate () {
     let tmp = this.width;
     this.width = this.height;
     this.height = tmp;
   }
 
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
