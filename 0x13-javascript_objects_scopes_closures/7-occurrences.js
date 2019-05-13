f5b87cb7bc4582ccb4d739faab4288a4eb736929
BennettDixon <bennettdixon16@gmail.com>
2019-05-13 17:45:15 +0000

    foreach loops in javascript!

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let i = 0;
+  list.forEach(function (obj) {
+    if (obj == searchElement) {
+      i++;
+    }
+  });
+  return i;
+};
