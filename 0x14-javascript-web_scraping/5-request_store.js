f46991301219e2a0d762ac0484f22d466132d853
BennettDixon <bennettdixon16@gmail.com>
2019-05-14 19:55:41 +0000

    storing data from the web using js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+const filesystem = require('fs');
+
+let url = process.argv[2];
+let filepath = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error != null) {
+    console.log(error);
+  } else {
+    filesystem.writeFile(filepath, body, 'utf8', function (err, data) {
+      if (error != null) {
+        console.log(error);
+      }
+    });
+  }
+});
