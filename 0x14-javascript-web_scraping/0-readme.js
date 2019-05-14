ad22d6c4040d2ba42455a6e77fe45954767a3468
BennettDixon <bennettdixon16@gmail.com>
2019-05-14 18:43:52 +0000

    opening files and reading them using nodejs

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const process = require('process');
+const filesystem = require('fs');
+
+let file = process.argv[2];
+
+filesystem.readFile(file, 'utf8', function(err, data) {
+  if (err != null) {
+    console.log(err);
+  } else {
+    process.stdout.write(data); 
+  }
+});
