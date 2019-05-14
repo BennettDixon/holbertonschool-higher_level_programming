9461460e1fd9e96f41801f3f4bf15a2b1a69828b
BennettDixon <bennettdixon16@gmail.com>
2019-05-14 18:47:01 +0000

    writing to files using nodejs

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+const filesystem = require('fs');
+
+let file = process.argv[2];
+let text = process.argv[3];
+
+filesystem.writeFile(file, text, 'utf8', function(err, data) {
+  if (err != null) {
+    console.log(err);
+  }
+});
