2191b99a3453c6854b88bd8c19eeafcae929ae49
BennettDixon <bennettdixon16@gmail.com>
2019-05-14 20:45:36 +0000

    dealing with asynchronous requests and ordering the results

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+let order = [];
+let responses = {};
+
+function getCharName (charUrl) {
+  let val;
+  val = request(charUrl, function (error, response, body) {
+    if (error != null) {
+      console.log(error);
+    } else {
+      let data = JSON.parse(body);
+      val = data['name'];
+      responses[charUrl] = val;
+    }
+  });
+}
+
+function doParse() {
+  let movie = process.argv[2];
+  let url = 'https://swapi.co/api/films/' + movie;
+
+  request(url, function (error, response, body) {
+    if (error != null) {
+      console.log(error);
+    } else {
+      let data = JSON.parse(body);
+      data['characters'].forEach(function (charUrl) {
+        order.push(charUrl);
+        getCharName(charUrl);
+      });
+    }
+  });
+}
+
+doParse();
+setTimeout(function(){
+  //some stuff
+  order.forEach(function (url) {
+    console.log(responses[url]);
+  });
+}, 5000);
