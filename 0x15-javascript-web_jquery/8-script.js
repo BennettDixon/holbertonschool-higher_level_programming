5d6b89793720e895011af12c07f1fed1fb133725
Bennett Dixon <benbendixon@gmail.com>
2019-05-16 12:50:46 -0700

    using 3rd party apis and dynamically displaying result using js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+const $ = window.$;
+const url = "https://swapi.co/api/films/?format=json";
+
+$.get(url, function(body) {
+  let films = body["results"];
+  let list = $("#list_movies");
+  films.forEach(element => {
+    list.append("<li>" + element["title"] + "</li>\n");
+  });
+  $("#character").html(name);
+});
